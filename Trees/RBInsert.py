652acf5119cbc34920bb54a7b7562a036b99f786
Nimish <nimish.v.shinde60@gmail.com>
2023-12-30 15:27:33 +0530

    Red Black Tree - week 3 Insert

---
+++ Trees/RBInsert.py
@@ -0,0 +1,43 @@
+class RBNode:
+    def __init__(self, val):
+        self.red = False
+        self.parent = None
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class RBTree:
+    def __init__(self):
+        self.nil = RBNode(None)
+        self.nil.red = False
+        self.nil.left = None
+        self.nil.right = None
+        self.root = self.nil
+
+    def insert(self, val):
+        new_node = RBNode(val)
+        new_node.parent = None
+        new_node.left = new_node.right = self.nil
+        new_node.red = True
+        current = self.root
+        parent = None
+        while current != self.nil:
+            parent = current
+            if new_node.val < current.val:
+                current = current.left
+            elif new_node.val > current.val:
+                current = current.right
+            else:
+                return
+
+        new_node.parent = parent
+        if not parent:
+            self.root = new_node
+        else:
+            if new_node.val < parent.val:
+                parent.left = new_node
+            else:
+                parent.right = new_node
+
+
