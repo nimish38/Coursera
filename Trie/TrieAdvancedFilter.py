f1c8c0b660929457d1c7f7c3fef9286f93c6ffe2
Nimish <nimish.v.shinde60@gmail.com>
2024-01-06 13:44:11 +0530

    Tries - Advanced word filter - week 4

---
+++ Trie/TrieAdvancedFilter.py
@@ -0,0 +1,44 @@
+class Trie:
+    def advanced_find_matches(self, document, variations):
+        matches = set()
+        for i in range(len(document)):
+            level = self.root
+            for j in range(i, len(document)):
+                ch = var = document[j]
+                if var in variations.keys():
+                    ch = variations[var]
+                if ch not in level:
+                    level = self.root
+                    break
+                level = level[ch]
+                if self.end_symbol in level:
+                    matches.add(document[i: j + 1])
+        return matches
+
+    # don't touch below this line
+
+    def find_matches(self, document):
+        matches = set()
+        for i in range(len(document)):
+            level = self.root
+            for j in range(i, len(document)):
+                ch = document[j]
+                if ch not in level:
+                    level = self.root
+                    break
+                level = level[ch]
+                if self.end_symbol in level:
+                    matches.add(document[i : j + 1])
+        return matches
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
