43867bd91a8b83376d34ba04699ff0f4271b30bb
Nimish <nimish.v.shinde60@gmail.com>
2024-01-10 20:22:19 +0530

    Traversal - Iterative Depth First Search - week 5

--- Traversal/DFS.py
+++ Traversal/DFS.py
@@ -1,12 +1,29 @@
 class Graph:
     def depth_first_search(self, start_vertex):
-        return self.depth_first_search_r([],start_vertex)
+        actual = self.depth_first_search_i([],start_vertex)
+        # expected = self.depth_first_search_r([],start_vertex)
+        # print(actual)
+        # print(expected)
+        return actual
 
     def depth_first_search_r(self, visited, current_vertex):
         visited.append(current_vertex)
+        x= sorted(self.graph[current_vertex])
         for neighbor in sorted(self.graph[current_vertex]):
             if neighbor not in visited:
                 self.depth_first_search_r(visited, neighbor)
+        return visited
+
+    def depth_first_search_i(self, visited, node):
+        stack = [node]
+        while len(stack) > 0:
+            vertex = stack.pop()
+            visited.append(vertex)
+            for neighbor in sorted(self.graph[vertex], reverse = True):
+                if neighbor not in visited:
+                    if neighbor in stack:
+                        stack.remove(neighbor)
+                    stack.append(neighbor)
         return visited
 
         # don't touch below this line
