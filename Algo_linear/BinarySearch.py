ec76bc737c4816dc167a47d5f52eb0dd210d3a90
Nimish <nimish.v.shinde60@gmail.com>
2024-01-16 20:50:58 +0530

    Algorithms - Binary Search Overflow prevention - week 6

--- Algo_linear/BinarySearch.py
+++ Algo_linear/BinarySearch.py
@@ -3,7 +3,7 @@ def binary_search(target, arr):
     high = len(arr) - 1
 
     while low <= high:
-        mid = (low + high) // 2
+        mid = low + (high-low)//2
         if arr[mid] == target:
             return True
         if target < arr[mid]:
