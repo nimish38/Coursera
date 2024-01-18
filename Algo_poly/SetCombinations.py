020404be9d02d4b6b193f3885ab4bf7f21db665a
Nimish <nimish.v.shinde60@gmail.com>
2024-01-18 19:19:40 +0530

    Algorithms - Permutations - week 6

--- Algo_poly/SetCombinations.py
+++ Algo_poly/SetCombinations.py
@@ -1,9 +1,17 @@
 def power_set(input_set):
+    final_res = subsets(input_set)
+    final_res.append([])
+    return final_res
+
+def subsets(input_set):
     resultSet = []
-    if len(input()) == 0:
+    if len(input_set) == 0:
         return resultSet
-    combos = power_set(input_set[1:])
+    combos = subsets(input_set[1:])
     for subset in combos:
-        firstCombo = subset.insert(input_set[0], 0)
-        resultSet.append(firstCombo, subset)
-    return resultSet
+        firstCombo = subset.copy()
+        firstCombo.insert(0, input_set[0])
+        resultSet.append(firstCombo)
+        resultSet.append(subset)
+    resultSet.append([input_set[0]])
+    return sorted(resultSet, key=lambda x: len(x), reverse=True)
